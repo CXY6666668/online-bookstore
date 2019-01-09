1e31998a38a5fd687bf2b748f105d2b9ef6663b5
Administrator <Administrator@PC201612062235>
2019-01-09 10:03:02 +0800

    简单提交一下，这就是说明

--- src/main/java/com/cxa/bookstore/controller/Test.java
+++ src/main/java/com/cxa/bookstore/controller/Test.java
@@ -1,13 +1,14 @@
 package com.cxa.bookstore.controller;
-/** 
 
-* @author 作者: 陈晓嫒 
-
-* @version 创建时间：2019年1月8日 下午9:50:11 
-
-* 类说明 :
-
-*/
+/**
+ * 
+ * @author 作者: 陈晓嫒
+ * 
+ * @version 创建时间：2019年1月8日 下午9:50:11
+ * 
+ *          类说明 :简单提交一下
+ * 
+ */
 public class Test {
 
 }
